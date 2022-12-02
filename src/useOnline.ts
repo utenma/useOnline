0c029e1d362acb9f26205974dc83adad02dfdeee
saulpalv <19735362+mindlid@users.noreply.github.com>
2022-12-02 11:50:32 -0700

    move setters inside of effect

--- src/useOnline.ts
+++ src/useOnline.ts
@@ -1,20 +1,19 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 
 export const useOnline = () => {
-    const [online, setOnline] = useState(navigator.onLine)
-
-    const onOnline = useCallback(() => setOnline(true), [])
-    const onOffline = useCallback(() => setOnline(false), [])
+    const [isOnline, setIsOnline] = useState(navigator.onLine)
 
     useEffect(() => {
+        const onOnline = () => setIsOnline(true)
+        const onOffline = () => setIsOnline(false)
         window.addEventListener('online', onOnline)
         window.addEventListener('offline', onOffline)
         return () => {
             window.removeEventListener('online', onOnline)
             window.removeEventListener('offline', onOffline)
         }
-    }, [onOffline, onOnline])
+    }, [])
 
-    return online
+    return isOnline
 }
 
